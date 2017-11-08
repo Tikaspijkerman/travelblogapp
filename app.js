5f669ab6d3c9dc562ea85f9e6fafd4b219c0cbb2
Tika Spijkerman <31164427+Tikaspijkerman@users.noreply.github.com>
2017-11-08 13:58:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var expressSanitizer = require("express-sanitizer");
 
 
 app.set("view engine", "ejs");
+app.set('port', process.env.PORT || 5000);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
